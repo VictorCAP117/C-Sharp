617de0d1abe2ef85da458f14ef7e3098e481face
Victor Santos <victor.alvessantos15@gmail.com>
2025-07-20 12:08:32 -0300

    Add files via upload

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nome = "Victor Santos";
+
+            Console.WriteLine("Hello, World!"); //para quebrar linha podemos usar o 'Line' (WriteLine)
+            Console.Write("Meu nome é Victor Santos!\n"); //Ou podemos usar \n no final do texto
+            Console.WriteLine(nome);
+            Console.Write(nome);
+            Console.ReadLine(); 
+        }
+    }
+}
