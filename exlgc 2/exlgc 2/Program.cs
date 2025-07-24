6376e73771d5885e9ba566bd90e8ea2ff8e6664e
Victor Santos <victor.alvessantos15@gmail.com>
2025-07-24 10:10:17 -0400

    Add files via upload

---
+++ exlgc 2/exlgc 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exlgc_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float valor = 0.0f;
+
+            Console.WriteLine("Digite um valor qualquer!");
+            valor = float.Parse(Console.ReadLine());
+            float out1 = valor - 1.0f;
+            Console.WriteLine(out1);
+            Console.ReadLine();
+        }
+    }
+}
