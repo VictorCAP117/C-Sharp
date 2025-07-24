013d84e6c1e4537eaebbb1aef617d3d6ca1056b0
Victor Santos <victor.alvessantos15@gmail.com>
2025-07-24 09:41:04 -0400

    Add files via upload

---
+++ exlgc 1/exlgc 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exlgc_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 10;
+            int B = 20;
+
+            Console.WriteLine($"A = {A}, B = {B}, inverta os valores!");
+            Console.Write("A = ");
+            A = int.Parse(Console.ReadLine());
+            Console.Write("B = ");
+            B = int.Parse(Console.ReadLine());
+            Console.WriteLine($"A = {A}");
+            Console.WriteLine($"B = {B}");
+
+            if (A == 20 && B == 10) {
+                Console.WriteLine("Parabéns, está correto!");
+            } else {
+                Console.WriteLine("Tente novamente!");
+            }
+        }
+    }
+}
