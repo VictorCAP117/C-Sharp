c4ebdf5eafe1cd04ff2ad869371c2cd1a31c069e
Victor Santos <victor.alvessantos15@gmail.com>
2025-07-25 10:27:33 -0400

    Add files via upload

---
+++ exlgc 4/exlgc 4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exlgc_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 0;
+            int meses = 0;
+            int dias = 0;
+            float totaldias = 0.0f;
+
+            Console.Write("Quantos anos você tem? ");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.Write($"{idade} anos e quantos meses? ");
+            meses = int.Parse(Console.ReadLine());
+
+            Console.Write($"{meses} meses e quantos dias? ");
+            int.TryParse(Console.ReadLine(), out dias);
+
+            totaldias = idade * 12 * 30.4166666667f + meses * 30.4166666667f + dias;
+
+            Console.WriteLine($"Você tem {totaldias} dias de vida.");
+
+        }
+    }
+}
